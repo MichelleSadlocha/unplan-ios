de358030b8cf2a29990b0da2e052f0266178dca7
Michelle van Deventer <michelle@luno.com>
2020-02-29 15:35:22 +0200

    Setup project

---
+++ Unplan/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Unplan
+//
+//  Created by Michelle Deventer on 2020/02/29.
+//  Copyright © 2020 Michelle Deventer. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
